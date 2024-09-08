4c5d5531b2b1a9d92bf80659454d19cf752418cb
singurty <singurty@gmail.com>
2024-09-07 21:00:17 -0400

    add static folder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from azure.storage.blob import BlobServiceClient
 from flask import Flask, flash, request, redirect, Response
 from werkzeug.utils import secure_filename
 import mimetypes
@@ -22,29 +22,19 @@ app = Flask(__name__)
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash("No file part")
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=filename)
-            blob_client.upload_blob(file)
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    if 'file' not in request.files:
+        flash("No file part")
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=filename)
+        blob_client.upload_blob(file)
 
 @app.route('/file/<name>')
 def download_file(name):
