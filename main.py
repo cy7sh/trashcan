61725b5738c55a08688d1c92f9d38aae13ff674d
singurty <singurty@gmail.com>
2024-09-10 21:12:49 -0400

    next time just stick to what works

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient
-from flask import Flask, flash, request, redirect, Response, render_template, abort, session
+from flask import Flask, flash, request, redirect, Response, render_template, session
 from werkzeug.utils import secure_filename
 from sqlalchemy import URL
 import mimetypes
@@ -11,10 +11,13 @@ from argon2 import PasswordHasher
 import random
 import string
 import os
+import struct
 
 import model
 
 CONTAINER_NAME = 'userfiles'
+SQL_COPT_SS_ACCESS_TOKEN = 1256  # Connection option for access tokens, as defined in msodbcsql.h
+TOKEN_URL = "https://database.windows.net/"  # The token URL for any Azure SQL database
 
 default_credential = DefaultAzureCredential()
 
@@ -26,7 +29,10 @@ app.config.from_prefixed_env()
 
 connection_string = os.getenv('AZURE_SQL_CONNECTIONSTRING')
 connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+token_bytes = default_credential.get_token(TOKEN_URL).token.encode('utf-16-le')
+token_struct = struct.pack(f'<I{len(token_bytes)}s', len(token_bytes), token_bytes)
 app.config['SQLALCHEMY_DATABASE_URI'] = connection_url
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"connect_args": {"attrs_before": {SQL_COPT_SS_ACCESS_TOKEN: token_struct}}}
 model.db.init_app(app)
 
 @app.route('/upload', methods=['POST'])
