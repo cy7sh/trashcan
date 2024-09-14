b2c3123d2fac5788e717269ec060d0f44e44c4c7
Shirish Pokhrel <shirish@shirish.ca>
2024-09-14 01:48:58 -0400

    configure logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 service ssh start
-exec gunicorn main:app > gunicorn.log 2> gunicorn_error.log
+exec gunicorn main:app
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,3 +3,5 @@ import multiprocessing
 bind = "0.0.0.0:80"
 workers = multiprocessing.cpu_count() * 2 + 1
 timeout = 0
+accesslog = "./gunicorn.access.log"
+errorlog = "./gunicorn.error.log"
